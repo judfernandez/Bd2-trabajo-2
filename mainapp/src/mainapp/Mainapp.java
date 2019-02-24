efc81dca1b801f76ac9af70c627d9c96167e07f5
scadavidg <31990704+scadavidg@users.noreply.github.com>
2019-02-24 18:21:05 -0500

    Main app

---
+++ mainapp/src/mainapp/Mainapp.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainapp;
+
+import java.util.Arrays;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author santi
+ */
+public class Mainapp {
+   public boolean pedidosent=false;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        interfaz DrawWindow = new interfaz();
+        DrawWindow.setSize(395,500);
+        DrawWindow.setResizable(false);
+        DrawWindow.setLocation(200, 50);
+        DrawWindow.setTitle("Pintando la ruta que viene de la BD");
+        DrawWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        DrawWindow.setVisible(true);
+        
+        
+
+        
+        
+        
+        
+    }
+
+    
+    
+    
+}
